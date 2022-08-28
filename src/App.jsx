c9c5c994acfc979780ed550d359638846ca7298a
Pete Seeling <peteseeling@gmail.com>
2022-08-28 13:22:54 -0400

    2 section of header complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
+import Header from "./views/Header/Header";
+
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+  <Header />
+    <hr></hr>
+    <h2>Main</h2>
+    <hr></hr>
+    <h3>footer</h3>
+    </>
+  )
 }
---
+++ src/views/Header/Header.jsx
@@ -0,0 +1,32 @@
+
+
+export default function Header(){
+    return(
+        <>
+        <div>
+        <section>
+        <span>
+            <p>Browse our complete catalog of OEM and licensed British heritage parts for your classic British automobile. | Kalam</p>
+        </span>
+        </section>
+
+        <section>
+            <h2>English Parts.com</h2>
+            <span>
+                <img src={require('../../../public/images/icons/header-location.png')} />
+                <p>5850 Stadium Dr, Kalamazoo, MI 49009</p>
+                <img src={require('../../../public/images/icons/header-phone.png')} />
+                <p>866.467.1776</p>
+            </span>
+            <span>
+
+            </span>
+        </section>
+        </div>
+        
+        
+        
+        </>
+    )
+
+}
