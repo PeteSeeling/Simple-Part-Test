96b4d101b7753b26fb5664750900923d7270e3a2
Pete Seeling <peteseeling@gmail.com>
2022-08-28 13:44:26 -0400

    BlueBox.jsx component created. Needs styling

---
+++ src/components/BlueBox/BlueBox.jsx
@@ -0,0 +1,9 @@
+
+export default function BlueBox(){
+    return(
+        <div>
+            <h2>Premium Austin Healey, Jaguar, MG, and Triumph Parts</h2>
+            <p>Unmistakable style. That's what you love about your Austin Healey, Jaguar, MG, or Triumph. At EnglishParts.com, we carry a large selection of premium repair, maintenance and restoration parts for classic British cars. You know you can count on us because we're located in Kalamazoo, MI. Finding the right premium repair parts has always been our first priority. Our state of the art premium parts diagrams ensure that you're getting the perfect part the first time. Our experienced team knows these cars inside and out, and is here to help. Excellence. The first time, every time.</p>
+        </div>
+    )
+}
