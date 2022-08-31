251d454f78298a5bf7d66f53ce56b5f88cbf0ff8
Pete Seeling <peteseeling@gmail.com>
2022-08-30 22:38:17 -0400

    header partially styled

--- src/views/Header/Header.jsx
+++ src/views/Header/Header.jsx
@@ -1,42 +1,57 @@
-
+import styles from '../../styles.less'
 
 export default function Header(){
 
     return(
         <>
-        <div>
+        <div className='header'>
         <section>
-        <span>
+        <span className='headerTop' >
             <p>Browse our complete catalog of OEM and licensed British heritage parts for your classic British automobile. | Kalam</p>
         </span>
         </section>
+<div className='headerBody'>
+        <section className='headerBodyUpper'>
+        <img className='britishFlag' src={require('../../../public/images/icons/british-flag.png')} />
+            <h2 >English Parts.com</h2>
+            <div className='partsMenu'>
 
-        <section>
-            <h2>English Parts.com</h2>
-            <span>
-                <img src={require('../../../public/images/icons/header-location.png')} />
-                <p>5850 Stadium Dr, Kalamazoo, MI 49009</p>
-                <img src={require('../../../public/images/icons/header-phone.png')} />
-                <p>866.467.1776</p>
-            </span>
-        </section>
+            <button className='partsButton'>Parts
+                <img src={require('../../../public/images/icons/default/arrow-down.png')} /></button>
 
-        <section>
-            <span>
-                <h4>Parts</h4>
-                <img src={require('../../../public/images/icons/default/arrow-down.png')} />
-                <h4> About Us</h4>
+               <button className='partsButton'> About Us
                 <img alt='arrow-right' src ={require('../../../public/images/icons/arrow-right.png')}/>
-            </span>
+                </button>
+            </div>
         </section>
 
-        <section>
+        <section className='headerLower'>
+            <span className='row'>
+
+                <button className='iconButton'>
+                   <img classname='icon' src={require('../../../public/images/icons/header-location.png')} />
+                <p>5850 Stadium Dr, Kalamazoo, MI 49009</p>
+                </button>
+
+                <button className='iconButton'>
+                <img classname='icon' src={require('../../../public/images/icons/header-phone.png')} />
+                <p>866.467.1776</p>
+                </button>
+
+                <button className='iconButton'>
+                   <img classname='icon' src={require('../../../public/images/icons/header-email.png')} />
+                <p>sales@englishparts.com</p>
+                </button>
+            </span>
+        </section>
+        </div>
+
+        <section className='headerSearch'>
             <span>
-                <input placeholder='Search by  Keywords, Part Numbers or Vin'>
+                <input className='input' placeholder='Search by  Keywords, Part Numbers or Vin'>
                 </input>
-                <button>GO</button>
+                <button className='searchButton'>GO</button>
                 <h4>Select Your Vehicle</h4>
-                <img src={require('../../../public/images/parts/tiles/austin-healey.png')} />
                 <img src={require('../../../public/images/icons/cart-empty.png')} />
             </span>
         </section>
